7eb14c2405b00ff1b743a8ce9ca438738ac09f39
itsrahul <rahul17a1997@gmail.com>
2020-04-04 11:27:24 +0530

    Update inputHint.js

--- exercises/js/inputHint.js
+++ exercises/js/inputHint.js
@@ -1,13 +1,16 @@
-let input = $("input[name=q]");
-let label = $("label[for=q]").remove().prop("textContent");
-
-input.val(label).addClass("hint")
-  .on("focus", function(){ 
-    input.val("").removeClass("hint");
-})
-  .on("blur", function(){
-    if(input.val().trim() == "")
-    {
-      input.val(label).addClass("hint");
-    }
+$(document).ready(function() {
+  
+  let input = $("input[name=q]");
+  let label = $("label[for=q]").remove().prop("textContent");
+  input.val(label).addClass("hint")
+    .on("focus", function(){ 
+      $(event.target).val("").removeClass("hint");
+  })
+    .on("blur", function(){
+      if($(event.target).val().trim() == "")
+      {
+        $(event.target).val(label).addClass("hint");
+      }
+  });
+  //insted of input use event 
 });
