f1f22ea369d75cb23059b19c6ac63efd83397d94
itsrahul <rahul17a1997@gmail.com>
2020-04-06 13:18:23 +0530

    Update navigation.js

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
+class HoverChildren
+{
+  constructor(parentElement)
+  {
+    this.childrenElements = $(parentElement).children();
+  }
 
-$("#nav").children().each(function(){
-  
-  $(this).hover(
-    function(){
-        $(this).addClass("li selected").find('ul').show();
-      
-    }, function(){
-      $(this).removeClass("selected").find('ul').hide();
+  addHover()
+  {
+    for(let item of this.childrenElements)
+    {
+      $(item).hover( () => this.hoverIn(item), () => this.hoverOut(item) );
     }
-  );
+  }
 
-})
+  hoverIn(item)
+  {
+    $(item).addClass("selected").find('ul').show();
+  }
+
+  hoverOut(item)
+  {
+    $(item).removeClass("selected").find('ul').hide();
+  }
+
+};
+
+let navBar = new HoverChildren("#nav");
+navBar.addHover();
 
 });
