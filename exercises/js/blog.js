ebc34d0c76f4b08e54008a4958731d957b8080d4
itsrahul <rahul17a1997@gmail.com>
2020-04-06 12:19:34 +0530

    Update blog.js

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-
   $(this).on("click","div#blog ul li", function() {
-    $(this).find(".excerpt").slideDown();
-    $(this).siblings().find(".excerpt").slideUp();
-  });
+    let blogListItem = $(this);
+    blogListItem.find(".excerpt").slideDown();
+    blogListItem.siblings().find(".excerpt").slideUp();
+  })
 });
