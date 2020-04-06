138bb99bf82d1be204e4e32d1fa56671fd1711a6
itsrahul <rahul17a1997@gmail.com>
2020-04-06 12:23:53 +0530

    Update load.js

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-  
+$(document).ready(function() {  
   $('div.module#blog li h3').each(function(){
 
-    $div_element = $('<div>').addClass("excerpt");
-    $(this).after($div_element);
-    $(this).data("location", $div_element)
+    let $div_element = $('<div>').addClass("excerpt");
+    let blogHeader = $(this)
+    blogHeader.after($div_element);
+    blogHeader.data("location", $div_element)
+
   })
   .on("click", function(event){
-  
-    let link = $(this).find("a").attr("href").replace('#', ' #');
-    let local_div = $(this).data("location");
+    
+    let blogHeader = $(this);
+    let link = blogHeader.find("a").attr("href").replace('#', ' #');
+    let local_div = blogHeader.data("location");
     local_div.load('data/' + link);
     event.preventDefault();
   })
