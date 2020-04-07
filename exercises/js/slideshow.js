69711e2221360e323a67f9e067fd585e97c612b0
itsrahul <rahul17a1997@gmail.com>
2020-04-07 11:38:51 +0530

    Update slideshow.js

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -1,52 +1,53 @@
-$(document).ready(function() {
-
-  class SlideShow
+class SlideShow
+{
+  constructor(listItem, position)
   {
-    constructor(listItem, position)
+    this.item = $(listItem);
+    this.main = $(position);
+  }
+  startSlideshow()
+  {
+    this.main.prepend(this.item);
+    this.item.children().each(function()
     {
-      this.item = $(listItem);
-      this.main = $(position);
+      $(this).hide();
+    });
 
-    }
-    startSlideshow()
-    {
-      this.main.prepend(this.item);
-      this.item.find("li").each(function(){
-        $(this).hide();
-      });
-
-      this.item.find("li:first").show().addClass("onDisplay");
-
-      this.runSlideshow();
-    }
-
-    runSlideshow()
-    {
-      setInterval( () => {
-        let displayed = $(this.item.find("li.onDisplay"));
-    
-        if(displayed.prop("nextElementSibling") == null)
-        {
-          this.displayNext(displayed, displayed.prevUntil("ul").last() );
-        }
-        else
-        {
-          this.displayNext(displayed, $(displayed.prop("nextElementSibling")) );
-        }
-    
-      }, 4000);
-    }
-
-    displayNext(hideItem, showItem)
-    {
-      hideItem.removeClass("onDisplay").fadeOut( function(){
-          showItem.fadeIn().addClass("onDisplay");
-        });
-    }
-    
+    this.item
+      .find("li:first")
+      .show()
+      .addClass("onDisplay");
+    this.runSlideshow();
   }
 
+  runSlideshow()
+  {
+    setInterval( () => {
+      let displayed = $(this.item.find("li.onDisplay"));
+      if(displayed.prop("nextElementSibling") == null)
+      {
+        this.displayNext(displayed, displayed.parent().prop("firstElementChild") );
+      }
+      else
+      {
+        this.displayNext(displayed, displayed.prop("nextElementSibling") );
+      }
+    }, 4000);
+  }
+
+  displayNext(hideItem, showItem)
+  {
+    hideItem
+      .removeClass("onDisplay")
+      .fadeOut()
+      .promise()
+      .done(function(){
+        $(showItem).fadeIn().addClass("onDisplay");
+      });
+  }
+}
+
+$(document).ready(function() {
   let slideshowList = new SlideShow("ul#slideshow", "div#main");
   slideshowList.startSlideshow();
- 
 })
