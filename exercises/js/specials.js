c2472a807b0d1f9e52e1adf864ffb50b690cfcba
itsrahul <rahul17a1997@gmail.com>
2020-04-07 11:38:26 +0530

    Update specials.js

--- exercises/js/specials.js
+++ exercises/js/specials.js
@@ -1,48 +1,48 @@
-$(document).ready(function() {
-
-  class GetData
+class GetData
+{
+  constructor(form)
   {
-    constructor(form)
-    {
-      this.specialsForm = $(form);      
-      this.localDiv = $("<div>");
-      this.specialsForm.find('li.buttons').remove();
-      this.localDiv.insertAfter(this.specialsForm);
-    }
+    this.specialsForm = $(form);      
+    this.localDiv = $("<div>");
+    this.specialsForm.find('li.buttons').remove();
+    this.localDiv.insertAfter(this.specialsForm);
+  }
 
-    displayResult(data, day)
+  displayResult(data, day)
+  {
+    if(day)
     {
-      if(day)
-      {
-        let offer = data[day];
-        let title = $("<h3>").text(`${offer.title}`);
-        let details = $("<p>").text(`${offer.text}`);
-        let img = $("<img>").prop("src", `${offer.image}`);
-        this.localDiv
-          .prop("innerHTML", "")
-          .append(title)
-          .append(details)
-          .append(img)
-          .css('color', offer.color);;
-      }
-    }
-
-    loadDataFrom(jsonUrl)
-    {
-      this.specialsForm.on("change", () =>
-      {
-        let day = $(event.target).val();
-        $.ajax({
-          url: jsonUrl,
-          type: "get",
-          dataType: "json",
-          success: (data) => {
-            this.displayResult(data, day);
-          }
-        });
-      });
+      let offer = data[day];
+      let title = $("<h3>").text(`${offer.title}`);
+      let details = $("<p>").text(`${offer.text}`);
+      let img = $("<img>").prop("src", `${offer.image}`);
+      this.localDiv
+        .prop("innerHTML", "")
+        .append(title)
+        .append(details)
+        .append(img)
+        .css('color', offer.color);
     }
   }
+
+  loadDataFrom(jsonUrl)
+  {
+    this.specialsForm.on("change", () =>
+    {
+      let day = $(event.target).val();
+      $.ajax({
+        url: jsonUrl,
+        type: "get",
+        dataType: "json",
+        success: (data) => {
+          this.displayResult(data, day);
+        }
+      });
+    });
+  }
+}
+
+$(document).ready(function() {
   let specials = new GetData("form[action='specials.html']");
   specials.loadDataFrom("data/specials.json");
 });
