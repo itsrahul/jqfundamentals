23debd516c725182b36093f98cc956e4a917a685
itsrahul <rahul17a1997@gmail.com>
2020-04-08 13:03:29 +0530

    Update slideshow.js

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -2,17 +2,17 @@ class SlideShow
 {
   constructor(details)
   {
-    this.main = details.displayElement;
-    this.item = details.slideshowElement;
+    this.$main = details.displayElement;
+    this.$item = details.slideshowElement;
     this.displayClass = details.className;
-    this.displayed = null;
+    this.$displayed = null;
     this.toBeDisplayed = null;
   }
 
   startSlideshow()
   {
-    this.main.prepend(this.item);
-    this.item.children()
+    this.$main.prepend(this.$item);
+    this.$item.children()
       .hide()
       .first().show().addClass(this.displayClass);
     this.runSlideshow();
@@ -20,25 +20,25 @@ class SlideShow
 
   runSlideshow()
   {
-    this.displayed = $(this.item.find("li." + this.displayClass));
+    this.$displayed = this.$item.find("li." + this.displayClass);
 
-    if(this.displayed.prop("nextElementSibling") == null)
+    if(this.$displayed.next()[0] == null)
     {
-      this.toBeDisplayed = this.displayed.parent().prop("firstElementChild");
+      this.toBeDisplayed = this.$displayed.parent().children().first();
     }
     else
     {
-      this.toBeDisplayed = this.displayed.prop("nextElementSibling");
+      this.toBeDisplayed = this.$displayed.next();
     }
 
-    this.displayed
+    this.$displayed
       .removeClass(this.displayClass)
-      .fadeOut()
+      .fadeOut(1000)
       .promise()
       .done(() => {
         $(this.toBeDisplayed)
           .addClass(this.displayClass)
-          .fadeIn()
+          .fadeIn(1000)
           .delay(4000)
           .promise()
           .done( this.runSlideshow());
